9763acd0b756bd2c336479e11f13ac22f408fc93
Lorena <cicek.lorena@gmail.com>
2023-02-09 18:46:41 +0100

    Klasa entitet

---
+++ TerminalAplikacija/src/cicek/model/Entitet.java
@@ -0,0 +1,27 @@
+package cicek.model;
+
+abstract class Entitet {
+	
+	private int sifra;
+	
+
+	public Entitet(int sifra) {
+		super();
+		this.sifra = sifra;
+	}
+
+	public Entitet() {
+		super();
+	}
+
+	public int getSifra() {
+		return sifra;
+	}
+
+	public void setSifra(int sifra) {
+		this.sifra = sifra;
+	}
+	
+	
+
+}
